a38e3d115660360328aab2e14b673158198165b5
Graham Kelly <gkgoat6700@gmail.com>
2022-07-22 19:37:52 -0400

    a

--- pull.sh
+++ pull.sh
@@ -1,2 +1,2 @@
-git subtree pull --prefix 3rdparty/ttyped https://github.com/lambda-11235/ttyped.git CoC
-git subtree pull --prefix 3rdparty/rootify https://github.com/gkgoat1/rootify.git main
+git subtree pull -P 3rdparty/ttyped https://github.com/lambda-11235/ttyped.git CoC
+git subtree pull -P 3rdparty/rootify https://github.com/gkgoat1/rootify.git main
